12c2e05901b39050e9180004205f49654b8c5238
Daniel Rauer <rauer@bytemine.net>
2013-01-29 00:23:42 +0100

    Add some custom sort functions, .e.g to sort by IP address.

--- init.rb
+++ init.rb
@@ -14,6 +14,7 @@ Redmine::WikiFormatting::Macros.register do
   desc "Macro for add javascript and css files for table sort work"
   macro :sortable_table do |obj, args|
     content_for :header_tags, javascript_include_tag( "tablesort.js", :plugin => 'sortable_table').html_safe
+    content_for :header_tags, javascript_include_tag( "customsort.js", :plugin => 'sortable_table').html_safe
     content_for :header_tags, stylesheet_link_tag( :default, :plugin => 'sortable_table').html_safe
     ""
   end
