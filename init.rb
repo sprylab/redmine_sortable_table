b17bf36963beae769f96782caca075e354570b42
Philip Schiffer <philip.schiffer@sprylab.com>
2013-08-26 08:49:38 +0200

    fix plugin naming to work with 2.3.2

--- init.rb
+++ init.rb
@@ -1,6 +1,5 @@
 #
-
-Redmine::Plugin.register :sortable_table do
+Redmine::Plugin.register :redmine_sortable_table do
   name 'Sortable table plugin'
   author 'Alexander Golovko'
   description ''
@@ -13,9 +12,9 @@ end
 Redmine::WikiFormatting::Macros.register do
   desc "Macro for add javascript and css files for table sort work"
   macro :sortable_table do |obj, args|
-    content_for :header_tags, javascript_include_tag( "tablesort.js", :plugin => 'sortable_table').html_safe
-    content_for :header_tags, javascript_include_tag( "customsort.js", :plugin => 'sortable_table').html_safe
-    content_for :header_tags, stylesheet_link_tag( :default, :plugin => 'sortable_table').html_safe
+    content_for :header_tags, javascript_include_tag( "tablesort.js", :plugin => 'redmine_sortable_table').html_safe
+    content_for :header_tags, javascript_include_tag( "customsort.js", :plugin => 'redmine_sortable_table').html_safe
+    content_for :header_tags, stylesheet_link_tag( :default, :plugin => 'redmine_sortable_table').html_safe
     ""
   end
 end
